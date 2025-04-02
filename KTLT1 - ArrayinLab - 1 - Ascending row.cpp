24e70e06f3271576f6aee3764886920eaa40fc78
nhandeeptrai123 <nanicude1234@gmail.com>
2025-04-02 10:08:17 +0700

    ascending row

---
+++ KTLT1 - ArrayinLab - 1 - Ascending row.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+int ascendingRows(int arr[][1000], int row, int col) {
+bool possible = false; 
+int rowF = 0;
+for (int i = 0; i < row; i++) {
+     for (int j = 0; j < col - 1; j++) {
+        if (arr[i][j] < arr[i][j+1]) {
+            possible = true;
+        }
+        else {
+            possible = false;
+            break;
+        }
+    }
+    if (possible == true) {
+        rowF++;
+    }
+}
+return rowF;
+return -1;
+}
+int main () {
+int arr[][1000] = {{32,4,9},{-80,37,71},{-91,-79,-55}};
+cout << ascendingRows(arr,3, 3);
+}
